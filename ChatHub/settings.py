e184f301128de8839012c5f55d432529215bff8d
Tayyab ur Rehman <113255822+tayyab2914@users.noreply.github.com>
2023-11-25 00:33:41 -0800

    Updated staticfinders

--- ChatHub/settings.py
+++ ChatHub/settings.py
@@ -50,11 +50,15 @@ INSTALLED_APPS = [
     'compressor',
 ]
 
-COMPRESS_ROOT = BASE_DIR / 'static'
+COMPRESS_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'compressed_static')
 
 COMPRESS_ENABLED = True
 
-STATICFILES_FINDERS = ('compressor.finders.CompressorFinder',)
+STATICFILES_FINDERS = (
+    'compressor.finders.CompressorFinder',
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -151,7 +155,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_ROOT = BASE_DIR / 'staticfiles_build' / 'static'
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
